7720648ffcb32fdee8206caede94eab7e3df9d61
URSUS32 <saul.razo@iteso.mx>
2025-09-04 15:48:17 -0600

    Initial Config

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 // Mantener imagen en src/assets/
 import logoUrl from '@/assets/bmagallanes_logo.png?url';
+import { Camera } from 'lucide-vue-next';
 
 </script>
 
@@ -8,8 +9,22 @@ import logoUrl from '@/assets/bmagallanes_logo.png?url';
     <!-- <header>
         <img :src="logoUrl" alt="Logo">
     </header> -->
+    <div class="fab">
+        <!-- a focusable div with tabindex is necessary to work on all browsers. role="button" is necessary for accessibility -->
+        <div tabindex="0" role="button" class="btn btn-lg btn-circle btn-primary">
+            <Camera color="#000000" :size="32" />
+        </div>
+
+        <!-- Main Action button replaces the original button when FAB is open -->
+        <div class="fab-main-action">
+            Main Action <button class="btn btn-circle btn-secondary btn-lg">M</button>
+        </div>
+
+        <!-- buttons that show up when FAB is open -->
+        <div>Label A <button class="btn btn-lg btn-circle">A</button></div>
+        <div>Label B <button class="btn btn-lg btn-circle">B</button></div>
+        <div>Label C <button class="btn btn-lg btn-circle">C</button></div>
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
