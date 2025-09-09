dd33b0526a90b34af3e01e8683cc776c76a96bbf
URSUS32 <saul.razo@iteso.mx>
2025-09-09 16:07:57 -0600

    Hero section update

--- src/components/pages/Home/HeroSection.vue
+++ src/components/pages/Home/HeroSection.vue
@@ -6,7 +6,7 @@ const { isMobile } = useMobileDetection(768);
 </script>
 
 <template>
-    <section class="h-dvh flex items-end justify-center">
+    <section class="h-screen h-dvh flex items-end justify-center">
         <div 
             class="w-[90%] h-[85%] mb-5 bg-cover bg-center bg-no-repeat rounded-3xl relative flex flex-col  items-start p-8 md:p-12 lg:p-16"
             :class="isMobile ? 'flex justify-end' : 'flex justify-center'"
