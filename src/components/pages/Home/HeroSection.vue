40935fdb2796e249a1f21a7a45712a87a23089b1
URSUS32 <saul.razo@iteso.mx>
2025-09-09 15:20:50 -0600

    Hero section update

--- src/components/pages/Home/HeroSection.vue
+++ src/components/pages/Home/HeroSection.vue
@@ -8,7 +8,7 @@ const { isMobile } = useMobileDetection(768);
 <template>
     <section class="h-screen flex items-end justify-center py-20">
         <div 
-            class="w-[90%] h-[85%] bg-cover bg-center bg-no-repeat rounded-3xl relative flex flex-col  items-start p-8 md:p-12 lg:p-16"
+            class="w-[90%] h-[90%] bg-cover bg-center bg-no-repeat rounded-3xl relative flex flex-col  items-start p-8 md:p-12 lg:p-16"
             :class="isMobile ? 'flex justify-end' : 'flex justify-center'"
             :style="{ backgroundImage: `url(${HeroImage})` }"
         >
@@ -16,9 +16,9 @@ const { isMobile } = useMobileDetection(768);
             <div class="absolute inset-0 bg-black/50 rounded-3xl"></div>
             
             <!-- Contenido sobre el fondo -->
-            <div class="relative z-10 " :class="isMobile ? 'mb-12': ''">
+            <div class="relative z-10 " :class="isMobile ? 'mb-8': ''">
                 <div class="mb-8">
-                    <h1 class="text-white text-5xl sm:text-6xl lg:text-7xl  font-light leading-tight mb-4 w-[60%]" :class="{ 'w-full': isMobile }">
+                    <h1 class="text-white text-4xl sm:text-5xl lg:text-6xl  font-light leading-tight mb-4 w-[60%]" :class="{ 'w-full': isMobile }">
                         El mejor pan tradicional de Guadalajara
                     </h1>
                     <p class="text-white text-2xl font-light opacity-90 w-[60%]" :class="{ 'w-full': isMobile }">
@@ -27,7 +27,7 @@ const { isMobile } = useMobileDetection(768);
                 </div>
                 
                 <div class="w-full" :class="isMobile ? 'flex justify-center' : 'flex justify-start'">
-                    <button class="bg-white px-12 py-4 text-3xl md:px-12 md:py-4 md:text-4xl lg:px-18 rounded-xl border-3 border-black hover:bg-gray-100 transition-all duration-200">
+                    <button class="bg-white px-8 py-4 text-2xl md:px-12 md:py-4 md:text-4xl lg:px-18 rounded-xl border-3 border-black hover:bg-gray-100 transition-all duration-200">
                         Conócenos
                     </button>
                 </div>
